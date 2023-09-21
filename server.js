ad78a880ca66299f7e0ba0710bd3a43f6be41cf3
Uchel <russel.emilian@gmail.com>
2023-09-22 04:25:19 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+function server(){
+	
+	let result = target([1,2,3,4,5,6,7,7,8,9],11)
+	for (let i = 0; i<result.length;i+=2){
+			console.log(result[i], result[i+1])
+	}
+}
+
+function target (arr, target){
+let result = []
+	for (let i = 0; i < arr.length; i++){
+		for (let j= i +1 ; j < arr.length; j++){
+			if(arr[i]+arr[j]=== target){
+			result.push(arr[i],arr[j])
+			}
+		}
+
+	}
+	return result
+}
+
+server()
